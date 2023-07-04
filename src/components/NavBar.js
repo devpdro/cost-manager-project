d8ae85400a505fc7523b8eb36eac8cbe096d7651
victor <victorpedr879@gmail.com>
2023-07-04 20:49:34 -0300

    feat: start navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <nav>
+        <div>
+          <img src="public/assets/costs_logo.png" alt="Logo" />
+        </div>
+        <ul>
+          <li>
+
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default App;
