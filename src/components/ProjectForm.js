86b75a5e3ba16a56fda2ea8521bbbb73a86c49c6
victor <victorpedr879@gmail.com>
2023-07-05 18:49:01 -0300

    feat: creating API with json

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import styles from "../styles/ProjectForm.module.css";
 
 import Input from "../components/Form/Input";
@@ -5,14 +7,28 @@ import Select from "../components/Form/Select";
 import Submit from "../components/Form/Submit";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      });
+  }, []);
+
   return (
-      <form className={styles.form}>
+    <form className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto"
         name="name"
         placeholder="Insira o nome do projeto"
-        
       />
       <Input
         type="number"
@@ -23,9 +39,10 @@ function ProjectForm({ btnText }) {
       <Select
         name="category_id"
         text="Selecione a categoria"
+        options={categories}
       />
       <Submit text={btnText} />
-      </form>
+    </form>
   );
 }
 
